8e5e5183b319ad603e4f54624b40340d037d8fbd
Mario <mariolopjr@users.noreply.github.com>
2025-11-22 16:34:54 -0500

    remove user-based packages

--- build_files/install-flatpaks.sh
+++ build_files/install-flatpaks.sh
@@ -5,8 +5,5 @@ echo "Installing flatpaks"
 
 flatpak install flathub --assumeyes \
     it.mijorus.gearlever \
-	md.obsidian.Obsidian \
-    org.gnome.World.PikaBackup \
-    io.github.neil_morrison44.pocket-sync \
-	dev.vencord.Vesktop
+    org.gnome.World.PikaBackup
 
